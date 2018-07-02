a7afcb5c50a6faaf7e4bef283088e9d3183b2785
Etch <Etch.rush@gmail.com>
2018-07-02 12:58:18 -0800

    home page

---
+++ assets/javascript/home.js
@@ -0,0 +1,14 @@
+
+$.ajax({
+    //headers: {
+    //"Client-ID": "y5d67zepc6iut00ks7u7gukvfirtvy"
+    //},
+    method: "GET",
+    url: "https://api.opendota.com/api/leagues"
+}).then(function (res) {
+
+    console.log(res)
+
+
+    
+});
